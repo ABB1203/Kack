b25495db245be355ed45b502bb510ac77ee2918d
Jastamo <bjorn.soderberg97@gmail.com>
2013-06-30 22:39:53 +0200

    Added health bars and non-square sprites
    The health bars are rendered over everything else. Currently they are
    shown for six seconds after its mob has been damaged. The colors of the
    health bars are calculated by comparing the max health of a mob with its
    current health.
    
    The yellow bar is a feature which may later be used so that the mobs can
    regain some of their lost health.
    
    Non-square sprites have also been added.
    
    I accidentally rolled back this commit so I rewrote the code. Hopefully
    there are no bugs in it, but something may have been missed.

--- Game/Game Source Code/src/game/gfx/Screen.java
+++ Game/Game Source Code/src/game/gfx/Screen.java
@@ -12,7 +12,7 @@ public class Screen {
 	private final Random random = new Random();
 	public int width, height;
 	private int xOffset, yOffset;
-	
+
 	private Game game;
 
 	public int[] pixels;
@@ -27,69 +27,107 @@ public class Screen {
 			tiles[i] = random.nextInt(0xffffff);
 		}
 	}
-	
+
 	public void clear() {
-		for(int i = 0; i < pixels.length; i++) {
+		for (int i = 0; i < pixels.length; i++) {
 			pixels[i] = 0;
 		}
 	}
-	
+
 	public void renderTile(int xPos, int yPos, Tile tile) {
 		xPos -= xOffset;
 		yPos -= yOffset;
-		
-		for(int y = 0; y < tile.getSize(); y++) {
+
+		for (int y = 0; y < tile.getHeight(); y++) {
 			int yAbsolute = y + yPos;
-			for(int x = 0; x < tile.getSize(); x++) {
+			for (int x = 0; x < tile.getWidth(); x++) {
 				int xAbsolute = x + xPos;
-				if(xAbsolute < -tile.getSize() || xAbsolute >= width || yAbsolute < 0 || yAbsolute >= height) break;
-				if(xAbsolute < 0) xAbsolute = 0;
-				pixels[xAbsolute + yAbsolute * width] = tile.sprite.pixels[x + y * tile.getSize()];
+				if (xAbsolute < -tile.getWidth() || xAbsolute >= width || yAbsolute < 0 || yAbsolute >= height) break;
+				if (xAbsolute < 0) xAbsolute = 0;
+				pixels[xAbsolute + yAbsolute * width] = tile.sprite.pixels[x + y * tile.getWidth()];
 			}
 		}
 	}
-	
+
 	public void renderMob(int xPos, int yPos, Mob mob) {
 		xPos -= xOffset;
 		yPos -= yOffset;
-		for(int y = 0; y < mob.getSprite().getSize(); y++) {
+		for (int y = 0; y < mob.getSprite().getHeight(); y++) {
 			int yAbsolute = y + yPos;
-			for(int x = 0; x < mob.getSprite().getSize(); x++) {
+			for (int x = 0; x < mob.getSprite().getWidth(); x++) {
 				int xAbsolute = x + xPos;
-				if(xAbsolute < -mob.getSprite().getSize() || xAbsolute >= width || yAbsolute < 0 || yAbsolute >= height) break;
-				if(xAbsolute < 0) xAbsolute = 0;
-				
-				pixels[xAbsolute + yAbsolute * width] = mob.getSprite().pixels[x + y * mob.getSprite().getSize()];
-				
+				if (xAbsolute < -mob.getSprite().getWidth() || xAbsolute >= width || yAbsolute < 0 || yAbsolute >= height) break;
+				if (xAbsolute < 0) xAbsolute = 0;
+
+				pixels[xAbsolute + yAbsolute * width] = mob.getSprite().pixels[x + y * mob.getSprite().getWidth()];
+
 			}
 		}
 	}
-	
+
 	public void renderProjectile(int xPos, int yPos, Projectile p) {
 		xPos -= xOffset;
 		yPos -= yOffset;
-		for (int y = 0; y < p.getSprite().getSize(); y++) {
+		for (int y = 0; y < p.getSprite().getHeight(); y++) {
 			int yAbsolute = y + yPos;
-			for (int x = 0; x < p.getSprite().getSize(); x++) {
+			for (int x = 0; x < p.getSprite().getWidth(); x++) {
 				int xAbsolute = x + xPos;
-				if(xAbsolute < -p.getSprite().getSize() || xAbsolute >= width || yAbsolute < 0 || yAbsolute >= height) break;
-				if(xAbsolute < 0) xAbsolute = 0;
+				if (xAbsolute < -p.getSprite().getWidth() || xAbsolute >= width || yAbsolute < 0 || yAbsolute >= height) break;
+				if (xAbsolute < 0) xAbsolute = 0;
 
-				int color = p.getSprite().pixels[x + y * p.getSprite().getSize()];
-				if(color != 0xffEE00AC)	pixels[xAbsolute + yAbsolute * width] = p.getSprite().pixels[x + y * p.getSprite().getSize()];
+				int color = p.getSprite().pixels[x + y * p.getSprite().getWidth()];
+				if (color != 0xffEE00AC) pixels[xAbsolute + yAbsolute * width] = p.getSprite().pixels[x + y * p.getSprite().getWidth()];
 			}
 		}
 	}
-	
+
+	public void renderHealthBar(int xPos, int yPos, Mob m) {
+
+		xPos -= xOffset;
+		yPos -= yOffset;
+		for (int y = 0; y < m.getHealthBarSprite().getHeight(); y++) {
+			int yAbsolute = y + yPos;
+			for (int x = 0; x < m.getHealthBarSprite().getWidth(); x++) {
+				int xAbsolute = x + xPos;
+
+				if (xAbsolute < -m.getHealthBarSprite().getWidth() || xAbsolute >= width || yAbsolute < 0 || yAbsolute >= height) break;
+				if (xAbsolute < 0) xAbsolute = 0;
+
+				int color = 0;
+
+				if (m.getHealthBarSprite().pixels[x + y * m.getHealthBarSprite().getWidth()] == 0xff00ffff) {
+					int span = m.getHealthBarSprite().getWidth();
+
+					if (m.getLastHealth() / m.getMaxHealth() >= (x * 1.0 / span * 1.0) || x <= 1) {
+
+						if (m.getHealth() / m.getMaxHealth() >= (x * 1.0 / span * 1.0) || x <= 1) color = 0xff00;
+						else color = 0xffff00;
+					}
+
+					else color = 0xff0000;
+
+					// System.out.println(m.getHealth() + " : " +
+					// m.getLastHealth() + " : " + m.getMaxHealth());
+
+				} else {
+					color = m.getHealthBarSprite().pixels[x + y * m.getHealthBarSprite().getWidth()];
+				}
+				// int color = m.getHealthBarSprite().pixels[x + y *
+				// m.getHealthBarSprite().getWidth()];
+				if (color != 0xffEE00AC) pixels[xAbsolute + yAbsolute * width] = color;
+			}
+		}
+	}
+
 	public void setOffset(int x, int y) {
 		xOffset = x;
 		yOffset = y;
 	}
-	
+
 	public int getXOffset() {
 		return xOffset;
 	}
-	
+
 	public int getYOffset() {
 		return yOffset;
 	}
--- Game/Game Source Code/src/game/gfx/Sprite.java
+++ Game/Game Source Code/src/game/gfx/Sprite.java
@@ -1,12 +1,16 @@
 package game.gfx;
 
+import game.Game;
+
 public class Sprite {
 
-	private int size;
+	private int width, height;
 	private int x, y;
 	public int[] pixels;
 	private SpriteSheet sheet;
-	
+	private Game game;
+	private int tileSize = game.getTileSize();
+
 	public static Sprite grass = new Sprite(16, 0, 0, SpriteSheet.tiles);
 	public static Sprite flower = new Sprite(16, 1, 0, SpriteSheet.tiles);
 	public static Sprite rock = new Sprite(16, 2, 0, SpriteSheet.tiles);
@@ -14,17 +18,39 @@ public class Sprite {
 	public static Sprite shot = new Sprite(16, 3, 0, SpriteSheet.tiles);
 	public static Sprite voidSprite = new Sprite(16, 0xff);
 	
+	public static Sprite healthBar = new Sprite(32, 16, 0, 10, SpriteSheet.tiles);
+	
+	public static Sprite[] health = {
+		new Sprite(32, 16, 0, 15, SpriteSheet.tiles),
+		new Sprite(32, 16, 0, 14, SpriteSheet.tiles),
+		new Sprite(32, 16, 0, 13, SpriteSheet.tiles),
+		new Sprite(32, 16, 0, 12, SpriteSheet.tiles),
+		new Sprite(32, 16, 0, 11, SpriteSheet.tiles)
+	};
+			
 	public Sprite(int size, int x, int y, SpriteSheet sheet) {
-		this.x = x * size;
-		this.y = y * size;
-		this.size = size;
+		this.x = x * tileSize;
+		this.y = y * tileSize;
+		width = size;
+		height = size;
 		this.sheet = sheet;
-		pixels = new int[size * size];
+		pixels = new int[width * height];
+		loadSprite();
+	}
+	
+	public Sprite(int width, int height, int x, int y, SpriteSheet sheet) {
+		this.x = x * tileSize;
+		this.y = y * tileSize;
+		this.height = height;
+		this.width = width;
+		this.sheet = sheet;
+		pixels = new int[width * height];
 		loadSprite();
 	}
 
 	public Sprite(int size, int color) {
-		this.size = size;
+		width = size;
+		height = size;
 		pixels = new int[size * size];
 		setColor(color);
 	}
@@ -36,14 +62,18 @@ public class Sprite {
 	}
 
 	private void loadSprite() {
-		for (int y = 0; y < size; y++) {
-			for (int x = 0; x < size; x++) {
-				pixels[x + y * size] = sheet.pixels[(this.x + x) + (this.y + y) * sheet.getSize()];
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				pixels[x + y * width] = sheet.pixels[(this.x + x) + (this.y + y) * sheet.getSize()];
 			}
 		}
 	}
 	
-	public int getSize() {
-		return size;
+	public int getWidth() {
+		return width;
+	}
+	
+	public int getHeight() {
+		return height;
 	}
 }
